300057721ee1a48fdaf70865de2f587aac128451
olmansansoefe <olman.personal@gmail.com>
2023-02-20 08:45:12 -0600

    ejemplo clase 4

---
+++ Clase3-Ejemplo-Codigo/resources/js/logica.js
@@ -0,0 +1,29 @@
+// declaración guardar o reservar un pequeño espacio en la memoria temporal
+// variable: espacio de memoria que puede cambiar su contenido a lo largo de la ejecición del programa
+// indica una variable global
+var title;
+// indica de variable estructural
+let subtitle;
+// constante es un espacio de memoria que una vez asignado no va a cambiar su valor
+const height = 200;
+
+// inicializar es dar valor a un espacio reservado de memoria
+title = 'Este es mi primer ejerciciod de js'
+subtitle = 'que emoción!'
+
+// JS es un lenguaje no tipado
+// Direntes tipos de datos
+// string = texto
+// number = números
+// array = listas 
+// boolean = verdadero o falso
+
+// array
+List = ['manzana', 'peras', 'sandia']
+
+// Objeto es la representanción de un elemento del mundo real
+var user = {
+    name: 'Olman',
+    lastname: 'Santamaría'
+}
+// JSON 
